bf894d147e57b88ebc1c7e25a8cb0418b3354a38
Jaslav789 <palacinka.papat0h@icloud.com>
2025-11-08 21:55:17 +0100

    Vytvoření GUI a základní parametry okna.

---
+++ teacher.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import psycopg2
+
+root = Tk()
+root.title('Škola a databáze.')
+root.geometry('300x280')
+root.resizable(False, False) # změna velikosti okna
+
+root.mainloop()

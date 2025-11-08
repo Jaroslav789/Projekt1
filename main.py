68a976efb600def541737de59b0bf9cda517cf89
Jaslav789 <palacinka.papat0h@icloud.com>
2025-11-08 21:26:45 +0100

    Funkce pro vložení dat.

--- main.py
+++ main.py
@@ -20,3 +20,21 @@ def create():
 	connection.commit()
 	# vypnutí připojeni
 	connection.close
+
+def insert_data():
+	connection = psycopg2.connect(
+		dbname='student',
+		user='postgres',
+		password='admin',
+		host='localhost',
+		port='5432'
+	)
+
+	cur = connection.cursor()
+	cur.execute('''INSERT INTO teacher(name, age, address)
+						  VALUES ('Magonagallova', 50, 'Bradavice')
+''')
+	connection.commit()
+	connection.close()
+
+insert_data()

3e412dff7968a2be22cf63a24e82369328ea42fe
Jaslav789 <palacinka.papat0h@icloud.com>
2025-11-08 22:06:19 +0100

    Vytvořen obecný label + name section.

--- teacher.py
+++ teacher.py
@@ -6,4 +6,15 @@ root.title('Škola a databáze.')
 root.geometry('300x280')
 root.resizable(False, False) # změna velikosti okna
 
+# LABELS, ENTRIES
+label_general = Label(root, text="Add data")
+label_general.grid(row=0, column=1)
+
+# name section
+label_name = Label(root, text="Name: ")
+label_name.grid(row=1, column=0)
+
+entry_name = Entry(root)
+entry_name.grid(row=1, column=1)
+
 root.mainloop()
